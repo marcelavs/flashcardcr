2b4f97881240dce73fb06569725db9dde9140a63
marcelavs <maxmxyalr@gmail.com>
2024-12-03 10:21:41 -0300

    arquivos flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual comédia romântica de 2001 mostra uma jovem advogada loira que desafia estereótipos ao ingressar em Harvard?',
+    'Legalmente Loira (Legally Blonde)'
+)
+
+criaCartao(
+    'Filmes',
+    'Em qual filme de 2003 uma jovem de 13 anos acorda repentinamente com 30 anos?',
+    'De Repente 30 (13 Going on 30)'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual comédia romântica de 1999 é baseada na peça A Megera Domada de Shakespeare?',
+    '10 Coisas que Eu Odeio em Você (10 Things I Hate About You)'
+)
+
+criaCartao(
+    'Filmes',
+    'Em Como Perder um Homem em 10 Dias, qual é o objetivo de Andie Anderson ao começar um relacionamento com Benjamin Barry?',
+    'Escrever um artigo sobre como afastar um homem em 10 dias'
+)
